0d39d87fd1bf3b83fe6ac8bc8b4bd4ab2c68f32d
NanduNanduz <nandunsb99@gmail.com>
2025-01-20 00:08:12 +0530

    created Navbar.jsx

--- EmployeeAppFrontend/src/components/Navbar.jsx
+++ EmployeeAppFrontend/src/components/Navbar.jsx
@@ -1,11 +1,33 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react'
+import Link from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-      
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar style={{ backgroundColor: "brown" }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              EmployeeApp
+            </Typography>
+
+            <Link to={"/employees"}>
+              <Button style={{ color: "white" }}>HOME</Button>{" "}
+            </Link>
+            <Link to={"/addemployees"}>
+              <Button style={{ color: "white" }}>ADDEMPLOYEE</Button>
+            </Link>
+            <Link to={"/"}>
+              <Button style={{ color: "white" }} onClick={handleLogout}>
+                LOGOUT
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
-  )
+  );
 }
 
 export default Navbar
