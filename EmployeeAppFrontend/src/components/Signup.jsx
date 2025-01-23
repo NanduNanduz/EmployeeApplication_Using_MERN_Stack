372464f7ea6e3fbf787a6cad50eeb70da1a58f3b
NanduNanduz <nandunsb99@gmail.com>
2025-01-23 21:41:23 +0530

    Updated Signup page style

--- EmployeeAppFrontend/src/components/Signup.jsx
+++ EmployeeAppFrontend/src/components/Signup.jsx
@@ -5,8 +5,15 @@ import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div style={{ margin: "8%", textAlign: "center" }}>
-      <Typography variant="h3" style={{ color: "Brown" }}>
+    <div
+      style={{
+        margin: "8%",
+        textAlign: "center",
+        backgroundColor: "#cff4d2",
+        padding: "30px",
+      }}
+    >
+      <Typography variant="h3" style={{ color: "#56c596" }}>
         EmployeeApp Register
       </Typography>
       <br />
@@ -37,12 +44,16 @@ const Signup = () => {
           ></TextField>
         </Grid>
         <Grid size={{ xs: 12, md: 12 }}>
-          <Button color="secondary" variant="outlined">
+          <Button
+            color="black"
+            variant="outlined"
+            style={{ backgroundColor: "#56c596" }}
+          >
             Register
           </Button>
         </Grid>
         <Grid size={{ xs: 12, md: 12 }}>
-          <Link to={"/"} style={{ color: "brown" }}>
+          <Link to={"/"} style={{ color: "#56c596" }}>
             <br />
             Already registered? Login here
           </Link>
