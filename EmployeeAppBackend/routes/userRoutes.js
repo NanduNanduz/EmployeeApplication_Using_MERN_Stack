1a4ed5b26231964b25b03715c45a21dbf3405398
NanduNanduz <nandunsb99@gmail.com>
2025-01-26 14:27:06 +0530

    updated the user routes with add user  post operation

--- EmployeeAppBackend/routes/userRoutes.js
+++ EmployeeAppBackend/routes/userRoutes.js
@@ -40,4 +40,29 @@ router.post("/login", async (req, res) => {
   }
 });
 
+
+
+
+
+router.post("/adduser", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const newUser = new userModel({
+      email,
+      password,
+      role: "Employee",
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create" });
+  }
+});
+
+
+
 module.exports = router;
